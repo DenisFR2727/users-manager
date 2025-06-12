c027af3b4ab44a6bfdfe6413249d910f43c54d68
Denys <dh92fr@gmail.com>
2025-06-12 13:04:42 +0300

    component optimization userList and create renderCell in folder utils

--- src/features/users/UsersList.tsx
+++ src/features/users/UsersList.tsx
@@ -5,14 +5,12 @@ import {
     TableBody,
     TableRow,
     TableCell,
-    Tooltip,
     Spinner,
     useDisclosure,
 } from '@heroui/react';
 import { useNavigate } from 'react-router';
 import { useGetUsersQuery } from '../../services/api';
 import { useCallback } from 'react';
-import { EyeIcon, EditIcon, DeleteIcon } from './icons/icons';
 import SkeletonTable from '../../components/Skeleton/Skeleton';
 
 import type { IUsers } from '../../types/types';
@@ -27,6 +25,7 @@ import { setEditUserValue, setSearch } from './UsersSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import RadioSelectRole from '../../components/Checkbox/RadioGroupSelect';
 import './style.scss';
+import { renderUserCell } from '../../utils/renderCell';
 
 function UsersList() {
     const dispatch = useAppDispatch();
@@ -38,81 +37,16 @@ function UsersList() {
     const navigate = useNavigate();
 
     const renderCell = useCallback(
-        (user: IUsers, columnKey: ColumnKey) => {
-            const cellValue = user[columnKey as keyof IUsers];
-
-            switch (columnKey) {
-                case 'index':
-                    return (
-                        <div className="text-center">
-                            {filteredUsers.indexOf(user) + 1}
-                        </div>
-                    );
-                case 'name':
-                    return <div className="text-center">{user.name}</div>;
-                case 'role':
-                    return (
-                        <div className="flex flex-col">
-                            <p className="text-bold text-sm capitalize ">
-                                {cellValue}
-                            </p>
-                        </div>
-                    );
-                case 'email':
-                    return (
-                        <div className="email_user_field text-center">
-                            {user.email}
-                        </div>
-                    );
-                case 'createdAt':
-                    return (
-                        <div className="date_user_field text-center">
-                            {user?.createdAt}
-                        </div>
-                    );
-                case 'actions':
-                    return (
-                        <div className="relative flex items-center gap-2 ">
-                            <Tooltip content="Details">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <EyeIcon
-                                        onClick={() =>
-                                            navigate(
-                                                `/users/${user.id}/${user.name}`
-                                            )
-                                        }
-                                    />
-                                </span>
-                            </Tooltip>
-                            <Tooltip content="Edit user">
-                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                    <Tooltip content="Edit user">
-                                        <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                                            <div
-                                                onClick={() => {
-                                                    editUser(user);
-                                                    onOpen();
-                                                }}
-                                            >
-                                                <EditIcon />
-                                            </div>
-                                        </span>
-                                    </Tooltip>
-                                </span>
-                            </Tooltip>
-                            <Tooltip color="danger" content="Delete user">
-                                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                                    <DeleteIcon
-                                        onClick={() => deleteUser(user.id)}
-                                    />
-                                </span>
-                            </Tooltip>
-                        </div>
-                    );
-                default:
-                    return cellValue;
-            }
-        },
+        (user: IUsers, columnKey: ColumnKey) =>
+            renderUserCell(
+                user,
+                columnKey,
+                navigate,
+                editUser,
+                deleteUser,
+                onOpen,
+                filteredUsers
+            ),
         [navigate, deleteUser, editUser, onOpen, filteredUsers]
     );
 
---
+++ src/utils/renderCell.tsx
@@ -0,0 +1,83 @@
+import type { NavigateFunction } from 'react-router';
+import type { ColumnKey } from '../features/types';
+import { EditIcon } from '../features/users/icons/EditIcon';
+import type { IUsers } from '../types/types';
+import { Tooltip } from '@heroui/react';
+import { EyeIcon } from '../features/users/icons/EyeIcon';
+import { DeleteIcon } from '../features/users/icons/DeleteIcon';
+
+export const renderUserCell = (
+    user: IUsers,
+    columnKey: ColumnKey,
+    navigate: NavigateFunction,
+    editUser: (user: IUsers) => void,
+    deleteUser: (id: string | undefined) => void,
+    onOpen: () => void,
+    filteredUsers: IUsers[]
+) => {
+    const cellValue = user[columnKey as keyof IUsers];
+
+    switch (columnKey) {
+        case 'index':
+            return (
+                <div className="text-center">
+                    {filteredUsers.indexOf(user) + 1}
+                </div>
+            );
+        case 'name':
+            return <div className="text-center">{user.name}</div>;
+        case 'role':
+            return (
+                <div className="flex flex-col">
+                    <p className="text-bold text-sm capitalize ">{cellValue}</p>
+                </div>
+            );
+        case 'email':
+            return (
+                <div className="email_user_field text-center">{user.email}</div>
+            );
+        case 'createdAt':
+            return (
+                <div className="date_user_field text-center">
+                    {user?.createdAt}
+                </div>
+            );
+        case 'actions':
+            return (
+                <div className="relative flex items-center gap-2 ">
+                    <Tooltip content="Details">
+                        <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                            <EyeIcon
+                                onClick={() =>
+                                    navigate(`/users/${user.id}/${user.name}`)
+                                }
+                            />
+                        </span>
+                    </Tooltip>
+                    <Tooltip content="Edit user">
+                        <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                            <Tooltip content="Edit user">
+                                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                                    <div
+                                        onClick={() => {
+                                            editUser(user);
+                                            onOpen();
+                                        }}
+                                    >
+                                        <EditIcon />
+                                    </div>
+                                </span>
+                            </Tooltip>
+                        </span>
+                    </Tooltip>
+                    <Tooltip color="danger" content="Delete user">
+                        <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                            <DeleteIcon onClick={() => deleteUser(user.id)} />
+                        </span>
+                    </Tooltip>
+                </div>
+            );
+        default:
+            return cellValue;
+    }
+};
